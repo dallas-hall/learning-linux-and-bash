a5f70dde953353eef83fd8c21b14bfedce524fc5
Dallas Hall <dallas.hall@example.com>
2019-03-30 15:34:19 +1100

    man command-name

--- Linux-Command-Line-And-Shell-Scripting-Bible/Chapter03/show-manual.sh
+++ Linux-Command-Line-And-Shell-Scripting-Bible/Chapter03/show-manual.sh
@@ -1,2 +1,4 @@
 man man
 echo "man command-name"
+echo "The bash manual is a quick reference and not a step by step guide. It may not seem useful at first but read the first paragraph or two of the command's DESCRIPTION."
+
