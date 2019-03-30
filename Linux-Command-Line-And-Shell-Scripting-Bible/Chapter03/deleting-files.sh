91a98d718d2915182237ac4388ee26ca8a6d1a5e
Dallas Hall <dallas.hall@example.com>
2019-03-30 16:35:15 +1100

    rm -rf shit-to-delete

---
+++ Linux-Command-Line-And-Shell-Scripting-Bible/Chapter03/deleting-files.sh
@@ -0,0 +1,7 @@
+echo "# There is no trashcan in the shell, so be careful what you delete. A good idea is doing ls first to be sure you are selecting the write files before using rm."
+echo "$ ls file-to-delete"
+echo "$ rm file-to-delete"
+echo "# -rf is used to delete sub-directories and force through any errors."
+echo "$ rm -rf files-to-delete"
+echo "# -i is used as a prompt to ask if you are sure about deleting."
+echo "$ rm -i files-to-delete"
