6aeff070d0f3d7e23ce8555c546b713612cec107
Dallas Hall <dallas.hall@example.com>
2019-03-30 18:58:59 +1100

    mount -t filesystem-type device-name /mount/path

---
+++ Linux-Command-Line-And-Shell-Scripting-Bible/Chapter04/mount-device-to-virtual-filesystem.sh
@@ -0,0 +1,2 @@
+echo "# The Linux filesystem combines all media disk into a single virtual directory. To add new media to this virtual directory, you need to mount it first."
+echo "$ mount -t filesystem-type device-name /mount/path"
