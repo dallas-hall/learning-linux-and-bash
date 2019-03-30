c300efbd735e4ec8d3f3fb4cc6cad996451768b3
Dallas Hall <dallas.hall@example.com>
2019-03-30 16:52:58 +1100

    less filename

---
+++ Linux-Command-Line-And-Shell-Scripting-Bible/Chapter03/less-is-more.sh
@@ -0,0 +1,4 @@
+echo "# more can only page forwards."
+echo "$ more filename"
+echo "# less can page forwards and backwards, supports regex, and can display a contents file before reading it entirely."
+echo "$ less filename"
