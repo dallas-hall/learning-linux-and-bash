8346f0a355a9b7e9c53d2af4c8782c3ab728728b
Dallas Hall <dallas.hall@example.com>
2021-04-09 15:53:12 +1000

    Basic fail2ban jail display script.

---
+++ fail2ban-status.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+## fail2ban-status.sh
+
+JAILS=`fail2ban-client status | grep "Jail list" | sed -E 's/^[^:]+:[ \t]+//' | sed 's/,//g'`
+for JAIL in $JAILS
+do
+  fail2ban-client status $JAIL
+done
