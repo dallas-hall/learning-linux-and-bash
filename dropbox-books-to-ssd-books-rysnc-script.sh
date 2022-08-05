63338d587341ce78b0d34ea856971f17d42a9173
Dallas Hall <dallas.hall@example.com>
2022-08-05 20:47:54 +1000

    Deleted project files.

--- dropbox-books-to-ssd-books-rysnc-script.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-rsync --archive --executability \
---progress --stats --ignore-errors \
---human-readable --force \
-/home/blindcant/Dropbox/Sync/Books/ /home/blindcant/Documents/Books
--- dropbox-keepassxc-to-home-dir-rsync-script.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-rsync --archive --executability \
---progress --stats --ignore-errors \
---human-readable --force \
-/home/dallas/Dropbox/Sync/KeePassXC/dallas.kdbx /home/dallas/Documents/KeePassXC/dallas-from-dropbox.kdbx
