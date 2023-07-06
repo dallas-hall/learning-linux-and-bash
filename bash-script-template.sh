9b1e2c6f56748066b739c1f6346803ed7a3c1a32
Dallas Hall <mr.dallas.hall@gmail.com>
2023-07-06 16:34:14 +1000

    Updated the template.

--- bash-script-template.sh
+++ bash-script-template.sh
@@ -6,7 +6,13 @@
 SCRIPT_NAME=$(basename "$BASH_SOURCE") 
 # Get script's path with BASH_SOURCE - https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
 SCRIPT_PATH="$( dirname -- "${BASH_SOURCE[0]}" )"
-
+SCRIPT_OUTPUT_PATH="$( dirname -- "${BASH_SOURCE[0]}" )/output"
+# ANSI color codes - https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+RED='\e[31m'
+GREEN='\e[32m'
+YELLOW='\e[33m'
+BLUE='\e[34m'
+NC='\e[0m'
 # Toggle debugging with boolean - https://stackoverflow.com/questions/2953646/how-can-i-declare-and-use-boolean-variables-in-a-shell-script
 GLOBAL_DEBUGGING=true
 
@@ -17,13 +23,6 @@ else
   set -euo pipefail
 fi
 
-
-# ANSI color codes
-RED='\e[31m'
-GREEN='\e[32m'
-YELLOW='\e[33m'
-NC='\e[0m'
-
 # Functions
 error() {
 	printf "${RED}[ERROR]${NC} %s\n" "$*"
@@ -31,19 +30,24 @@ error() {
 }
 
 step() {
-	printf "${GREEN}[STEP]${NC} %s\n" "$*"
+	printf "[STEP] %s\n" "$*"
 }
 
 info() {
-	printf "[INFO] %s\n" "$*"
+	printf "${GREEN}[INFO]${NC} %s\n" "$*"
 }
 
 warn() {
 	printf "${YELLOW}[WARN]${NC} %s\n" "$*"
 }
 
+debug() {
+	printf "${BLUE}[WARN]${NC} %s\n" "$*"
+}
+
 # Tests
-step "Testing the step message."
 info "Testing the info message."
+step "Testing the step message."
 warn "Testing the warn message."
+debug "Testing the debug message."
 error "Testing the error message."
